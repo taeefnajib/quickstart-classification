26aa51e679b498777616c3ab25053fbc70f38acf
Taeef Najib <taeefnajib@gmail.com>
2023-06-08 18:37:42 +0000

    updated for st

--- __sidetrek__/project/wf_81_451/__bentoml__/save_model.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import bentoml
-with open("/userRepoData/taeefnajib/quickstart-classification/__sidetrek__/models/99604e83117863c6d9ac404493117504.joblib", 'rb') as f:
-    model = joblib.load(f)
-    saved_model = bentoml.sklearn.save_model(
-        "quickstart_model",
-        model,
-    )
-    print(saved_model) # This is required!
